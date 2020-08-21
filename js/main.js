60f8ba78c15a8887c6e64abeeefd2ed7fc653196
Maslov Anton <tosha.maslov.1999@mail.ru>
2020-08-21 15:31:49 +0300

    complete

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-$(document).ready(function () {
-    $('.slider').slick({        
+const swup = new Swup();
+init();
+swup.on('contentReplaced', init);
+function init() {
+    $('.slider').slick({
         prevArrow: '<button type = "button" class = "slick-prev"><img src="images/prevArrow.svg"></button>',
         nextArrow: '<button type = "button" class = "slick-next"><img src="images/nextArrow.svg"></button>'
     });
-});
+}
+
+
+
