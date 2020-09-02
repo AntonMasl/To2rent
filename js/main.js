d0a9f23bdcfee242f40e772c117c04c451206ee2
Maslov Anton <tosha.maslov.1999@mail.ru>
2020-09-02 20:34:37 +0300

    optimization

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
-const swup = new Swup();
-init();
-swup.on('contentReplaced', init);
-function init() {
+$(function () {
     $('.slider').slick({
         prevArrow: '<button type = "button" class = "slick-prev"><img src="images/prevArrow.svg"></button>',
         nextArrow: '<button type = "button" class = "slick-next"><img src="images/nextArrow.svg"></button>'
     });
-}
+});
 
 
 
